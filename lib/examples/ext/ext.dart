dc4351e3e275ee825b3f01de1c6207c895e9218a
ggdream <gdream@yeah.net>
2021-10-17 18:20:45 +0800

    format code

--- lib/examples/ext/ext.dart
+++ lib/examples/ext/ext.dart
@@ -3,9 +3,14 @@ import 'package:get/get.dart';
 
 import 'package:fldome/router/router.dart';
 
-class ExtPage extends StatelessWidget {
+class ExtPage extends StatefulWidget {
   const ExtPage({Key? key}) : super(key: key);
 
+  @override
+  _ExtPageState createState() => _ExtPageState();
+}
+
+class _ExtPageState extends State<ExtPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +21,13 @@ class ExtPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-      ListTile(
-        title: Text('chat'),
-        onTap: (){
-          Get.toNamed(RoutesNamespace.Chat);
-        },
-        trailing: Icon(Icons.arrow_forward_ios_rounded),
-      ),
+          ListTile(
+            title: Text('chat'),
+            onTap: () {
+              Get.toNamed(RoutesNamespace.Chat);
+            },
+            trailing: Icon(Icons.arrow_forward_ios_rounded),
+          ),
         ],
       ),
     );
